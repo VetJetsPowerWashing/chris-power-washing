e56b3f578a7c08f4a84cd698813466efa7283c37
VetJetsPowerWashing <162942579+VetJetsPowerWashing@users.noreply.github.com>
2024-08-06 21:03:31 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,25 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10945209180"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-10945209180');
-      </script>
       <body className={`${anton.className} flex flex-col min-h-screen`}>
         <Navbar />
         {children}
         <Footer />
-        <script>
-          window.addEventListener('click', function(e) {
-          if(e.target.closest('[href*="tel:"]')){
-          gtag('event', 'conversion', {'send_to': 'AW-10945209180/ARgjCPb82cgZENzGiuMo'});
-          }
-          });
-      </script>
       </body>
     </html>
   );
